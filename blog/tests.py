48ad16419ca71fd1e4bce0e63a0b1aca67137222
Ben Gray <ben.gray00@gmail.com>
2020-08-09 11:40:42 +0100

    Updated Testing

--- blog/tests.py
+++ blog/tests.py
@@ -1,21 +1,69 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-from blog.views import post_list
+from blog.views import post_list, cv, cv_new
 
 # Create your tests here.
 
+# Test CV page and test New CV Entry page
 
-class SmokeTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, post_list)
 
-    def test_home_page_returns_correct_html(self):
+class CVTest(TestCase):
+    def test_cv_page_displays_correct_sections(self):
+        # Name, Bio, Sections
         request = HttpRequest()
-        response = post_list(request)
+        response = cv(request)
         html = response.content.decode('utf8').strip()
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>Ben Gray\'s Django Coursework Blog</title>',
                       html)
+
+        # Check HTML for:
+        #   1. Title
+        self.assertIn('<h1>Ben Gray</h1>', html)
+
+        #   2. Each Section
+        self.assertIn('<div id="section-education">', html)
+        self.assertIn('<div id="section-work">', html)
+        self.assertIn('<div id="section-skills">', html)
+        self.assertIn('<div id="section-interests">', html)
+        self.assertIn('<div id="section-misc">', html)
+
+        self.assertTrue(html.endswith('</html>'))
+
+    def test_cv_form_includes_correct_fields(self):
+        # Form Fields
+        request = HttpRequest()
+        response = cv_new(request)
+        html = response.content.decode('utf8').strip()
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Ben Gray\'s Django Coursework Blog</title>',
+                      html)
+
+        # Check HTML for:
+
+        #   1. Header
+        self.assertIn('<h2>New CV Entry</h2>', html)
+
+        #   2. Sections
+        self.assertIn('<li>Bio (Overwrites)</li>', html)
+        self.assertIn('<li>Education</li>', html)
+        self.assertIn('<li>Work Experience</li>', html)
+        self.assertIn('<li>Additional Skills/Achievements</li>', html)
+        self.assertIn('<li>Interests</li>', html)
+        self.assertIn('<li>Miscellaneous</li>', html)
+
+        #   3. Section Input
+        self.assertIn('id="id_section"', html)
+
+        #   4. Text Input
+        self.assertIn('id="id_text"', html)
+
+        #   5. Date Inputs
+        self.assertIn('id="id_start_date"', html)
+        self.assertIn('id="id_end_date"', html)
+
+        #   6. Submit Button
+        self.assertIn('<button type="submit" class="save', html)
+
         self.assertTrue(html.endswith('</html>'))
--- functional_tests.py
+++ functional_tests.py
@@ -1,24 +1,87 @@
 from selenium import webdriver
 import unittest
+import time
 
 
-class NewVisitorTest(unittest.TestCase):
+class CVTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
-        self.browser.get('http://localhost:8000')
+    def admin_login(self):
+        # Log in to admin to enable write/edit
+        self.browser.get("http://localhost:8000/admin")
+        self.browser.find_element_by_id("id_username").send_keys("ben")
+        self.browser.find_element_by_id("id_password").send_keys(
+            "k3$*Z^8EgSFiHfc^R^$T")
+        self.browser.find_element_by_tag_name("form").submit()
+        time.sleep(1)
 
-        # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+    def test_cv_form(self):
+        # Test adding an item to the cv
 
-        # She is invited to enter a to-do item straight away
+        # Login to admin
+        self.admin_login()
+
+        # User loads the correct page
+        self.browser.get('http://localhost:8000/cv/new')
+
+        # User sees the title allows them to create a new item
+        title_text = self.browser.find_element_by_tag_name('h2').text
+        self.assertIn("New", title_text)
+
+        # User reads section numbers
+        header_text = self.browser.find_element_by_tag_name('h4').text
+        self.assertIn("Sections", header_text)
+
+        section_list = self.browser.find_element_by_tag_name(
+            'ol').get_attribute('innerHTML')
+        for section in [
+                "Bio", "Education", "Work", "Skills", "Interests", "Misc"
+        ]:
+            self.assertIn(section, section_list)
+
+        # User is invited to input a section number
+        section_input = self.browser.find_element_by_id("id_section")
+        section_input.send_keys("2")
+
+        entry_text = "Ecotricity - Bill Resolution Specialist"
+
+        # User inputs some text
+        text_input = self.browser.find_element_by_id("id_text")
+        text_input.send_keys(entry_text)
+
+        # User inputs a start and end date
+        start_date_input = self.browser.find_element_by_id("id_start_date")
+        start_date_input.send_keys("dd/mm/yyyy")
+
+        end_date_input = self.browser.find_element_by_id("id_end_date")
+        end_date_input.send_keys("dd/mm/yyyy")
+
+        # Check the user can submit the form
+        save_button = self.browser.find_elements_by_class_name("save")
+        self.assertTrue(len(save_button) > 0)
+
+        # User submits form data
+        self.browser.find_element_by_tag_name("form").submit()
+
+        # Wait for entry to be added
+        time.sleep(1)
+
+        # User goes to CV page
+        self.browser.get("http://localhost:8000/cv")
+
+        # User finds correct section
+        section = self.browser.find_element_by_id("section-work")
+        self.assertTrue(
+            section.find_element_by_tag_name("h4").text == "Work Experience")
+
+        # User reads recently inputted item under correct section
+        section_entries = section.find_elements_by_tag_name("li")
+        self.assertTrue(
+            any(entry.text == entry_text for entry in section_entries))
 
 
 if __name__ == '__main__':
